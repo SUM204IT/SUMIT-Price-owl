b57b8a74c83cd9142ffa1c521ed56933e91c156f
SUMIT MADDESHIYA <sumitmaddeshiya99@gmail.com>
2026-06-24 17:52:41 +0530

    added backend url

--- Frontend/src/services/auth.api.js
+++ Frontend/src/services/auth.api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:5000",
+    // baseURL:"http://localhost:5000",
+    baseURL:"https://sumit-price-owl.onrender.com",
     withCredentials: true
 })
 
